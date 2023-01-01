6107c61c80ad4615e8eeffc937825e16e69d3a42
Priyanshu Shrivastav <official.mr.convict@gmail.com>
2023-01-01 13:23:16 +0000

    Update city.js and index.js

--- api/src/db/city.js
+++ api/src/db/city.js
@@ -2,12 +2,13 @@ const createCityModel = (db) => {
   console.log(`[INFO]: Total cities in database: ${db.length}`);
   return {
     get({ offset, limit }) {
+      const timeStamp = new Date().toLocaleTimeString();
       console.log(
-        `[INFO: ${new Date().toLocaleTimeString()}]: Queries for getCities | offset: {${offset}}, limit: {${limit}} | `
+        `[INFO: ${timeStamp}]: Queries for getCities | offset: {${offset}}, limit: {${limit}} | `
       );
       if (offset >= db.length) {
         console.warn(
-          `[WARN]: offset exceeding the length | offset: {${offset}} total cities: {${db.length}} | `
+          `[WARN ${timeStamp}]: offset exceeding the length | offset: {${offset}} total cities: {${db.length}} | `
         );
         return {
           data: [],
@@ -16,7 +17,7 @@ const createCityModel = (db) => {
       }
       if (offset + limit >= db.length) {
         console.warn(
-          `[WARN]: offset+limit exceeding the length, expect less data | cities sent ${limit}} | `
+          `[WARN ${timeStamp}]: offset+limit exceeding the length, expect less data | cities sent ${limit}} | `
         );
         limit = db.length - offset;
       }
--- api/src/db/index.js
+++ api/src/db/index.js
@@ -1,12 +1,9 @@
 const { city: db } = require('./cityDB');
 const createCityModel = require('./city');
 
-const dbSliced = db.slice(0, 100);
-console.log(dbSliced.length);
-
 module.exports = {
   models: {
-    City: createCityModel(dbSliced),
+    City: createCityModel(db),
   },
-  db: dbSliced,
+  db,
 };
